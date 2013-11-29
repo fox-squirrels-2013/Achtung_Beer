7cb8fc20d28183b7916b2da87c3c3ac6c3332a02
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-29 11:10:14 -0800

    Deleted rails location test. Merging all information into rails-api.

--- test_location_gem/app/models/location.rb
+++
@@ -1,5 +0,0 @@
-class Location < ActiveRecord::Base
-  validates_presence_of :latitude, :longitude
-  geocoded_by :full_street_address
-  after_validation :geocode
-end
--- test_location_gem/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestLocationGem::Application.config.secret_token = '61b979830ad5f80e79351382ccfcd1766e16c85fc1335d5da160768f0ed5aec90d6044124ecf6666169f5c760a4d3c2e66062ee7736256e6b9fe994aded638e5'
